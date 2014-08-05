25e5a620657c4ef3b75893df0d3e351dad3fc101
eanplatter <ean@eanplatter.com>
2014-08-05 16:47:31 -0600

    modular stuff

--- public/js/myController.js
+++ public/js/myController.js
@@ -3,17 +3,21 @@ var app = angular.module('app');
 app.controller('MainController', function($scope, myService) {
 	$scope.test = "This is a test";
 	$scope.friends = myService.getFriends();
-	$scope.omg = function() {
+	var omg = function() {
+		var theData = 
 		$scope.friends.then(function(results) {
-			console.log(results)
-      		return results;
-    	})
+			var themFacts = results;
+			console.log(results);
+			console.log( "facts" + themFacts);
+      		return themFacts;
+    	});
+    	return theData;
 	};
 
-	$scope.real = $scope.omg();
+	$scope.real = omg();
 
 	$scope.testy = function() {
-		console.log($scope.omg());
+		console.log($scope.real);
 	};
 
 	$scope.getThoseFriends = function() {
